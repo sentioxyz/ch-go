a0303021d16e81c438d0d222a88ef89aa037a542
BarmBamos <barmbamos@gmail.com>
2025-10-25 07:44:53 +0000

    feat(decimal): add ColDecimal512 type and associated tests
    - Introduced ColDecimal512 type with methods for row management, encoding, and decoding.
    - Added tests for ColDecimal512 and ColDecimal512Array, including benchmarks for performance.
    - Created golden files for expected output during encoding and decoding processes.
    - Updated ch-gen-col tool to support Decimal512 type generation.

--- proto/decimal_test.go
+++ proto/decimal_test.go
@@ -5,5 +5,9 @@ func Decimal128FromInt(v int) Decimal128 {
 }
 
 func Decimal256FromInt(v int) Decimal256 {
-	return Decimal256(Int256FromInt(v))
+    return Decimal256(Int256FromInt(v))
+}
+
+func Decimal512FromInt(v int) Decimal512 {
+    return Decimal512(Int512FromInt(v))
 }
